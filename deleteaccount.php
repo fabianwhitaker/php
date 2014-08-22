e39bf65108f1a741cbf9daddb29dcfeee79c28b3
fjw2000 <fabianwhitaker@rocketmail.com>
2014-08-23 00:48:35 +0100

    Create deleteaccount.php
    Delete account previously made in formhandle.php

---
+++ deleteaccount.php
@@ -0,0 +1,28 @@
+<?php 
+// this file works on the premise that a session already exists, containing the userID, named uID.
+// get the user's ID from the session created upon login.
+$uID = $_SESSION['uID'];
+
+//connect to MySQL
+      DEFINE ('DB_USER', 'root');
+      DEFINE ('DB_PASSWORD', '');
+	  DEFINE ('DB_HOST', 'localhost');
+	  DEFINE ('DB_NAME', 'site');
+$dbc = @mysqli_connect (DB_HOST, DB_USER, DB_PASSWORD, DB_NAME) 
+       OR die("could not connect");
+mysqli_set_charset($dbc, 'utf8');
+
+//check the user still exists
+$check = "SELECT username FROM users WHERE userID='uID'";
+$r = mysqli_query($dbc, $check);
+$row = mysqli_fetch_assoc($r); 
+if (isset($row)) {
+     // carry on the program
+} else {
+     die("Sorry, the account you tried to delete does not exist.");
+}
+
+//delete the user's account
+$del = "DELETE FROM users WHERE userID='uID'";
+$query = mysqli_query($dbc, $del)or die("Sorry, we could not delete your account at this time.");
+print("Your account has been deleted, please contact us if your experience with the site has not been satisfactory.");
