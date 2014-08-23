8373ec0ea8dad05279e5fe3a221069fe6da4a8a4
fjw2000 <fabianwhitaker@rocketmail.com>
2014-08-23 10:53:10 +0100

    Update loginhandle.php

--- loginhandle.php
+++ loginhandle.php
@@ -1,7 +1,7 @@
 <?php
 //connect to MySQL
-      DEFINE ('DB_USER', 'root');
-      DEFINE ('DB_PASSWORD', '');
+          DEFINE ('DB_USER', 'root');
+          DEFINE ('DB_PASSWORD', '');
 	  DEFINE ('DB_HOST', 'localhost');
 	  DEFINE ('DB_NAME', 'party');
 $dbc = @mysqli_connect (DB_HOST, DB_USER, DB_PASSWORD, DB_NAME) 
